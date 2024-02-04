a1d06778646637f6e33bbc17475e57ebd036734d
Brett Sodie <blsodie@gmail.com>
2024-02-04 14:16:12 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
+import screenfull from 'screenfull';
 import './App.css';
 
 function App() {
+  const playerRef = React.useRef() as any;
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  // const onToggleFullscreen = () => {
+  //   const vid = document.querySelector('video');
+  //   screenfull.request(vid as any);
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactPlayer
+        controls={false}
+        ref={playerRef}
+        url='http://localhost:4000/index.m3u8'
+        playing={true}
+        loop={false}
+        width="100%"
+        height="auto"
+        config={{
+          file: {
+            hlsOptions: {
+              debug: true,
+            }
+          }
+        }}
+      />
     </div>
   );
 }
